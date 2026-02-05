b4cd7303afd5d93fdc095f737de7704624ac69b1
vincemichaelaquilizan <vincemichaelaquilizan@gmail.com>
2026-02-05 14:49:51 +0800

    update2

--- app/app.vue
+++ app/app.vue
@@ -3,29 +3,3 @@
     <NuxtPage />
   </UApp>
 </template>
-
-<script setup>
-import { useHead } from '#app'
-
-useHead({
-  title: 'freeQR',
-  meta: [
-    { name: 'description', content: 'free qr generator without any accounts' },
-
-    // Open Graph / Facebook
-    { property: 'og:title', content: 'freeQR' },
-    { property: 'og:description', content: 'free qr generator without any accounts' },
-    { property: 'og:image', content: '/image/image.png' },
-    { property: 'og:type', content: 'website' },
-
-    // Twitter
-    { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: 'freeQR' },
-    { name: 'twitter:description', content: 'free qr generator without any accounts' },
-    { name: 'twitter:image',content: '/image/image.png'  },
-  ],
-  link: [
-    { rel: 'icon', type: 'image/png', href: '/image/icon.png' }
-  ]
-})
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,24 @@ export default defineNuxtConfig({
         {
           name: 'google-site-verification',
           content: 'dEOs9uu8NQ1N1lKve-kI82M3XzmhLk0FQNn0TDFBT3w'
-        }
+        },
+         { name: 'description', content: 'free qr generator without any accounts' },
+
+        // Facebook / Open Graph
+        { property: 'og:url', content: 'https://freeqr.vincemichaelaquilizan.workers.dev/' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: 'freeQR' },
+        { property: 'og:description', content: 'free qr generator without any accounts' },
+        { property: 'og:image', content: 'https://freeqr.vincemichaelaquilizan.workers.dev/image/image.png' },
+
+        // Twitter
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { property: 'twitter:domain', content: 'freeqr.vincemichaelaquilizan.workers.dev' },
+        { property: 'twitter:url', content: 'https://freeqr.vincemichaelaquilizan.workers.dev/' },
+        { name: 'twitter:title', content: 'freeQR' },
+        { name: 'twitter:description', content: 'free qr generator without any accounts' },
+        { name: 'twitter:image', content: 'https://freeqr.vincemichaelaquilizan.workers.dev/image/image.png' }
+  
       ]
     }
   },
