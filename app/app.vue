5d34494de3a36da155b458919427de95eac56940
vincemichaelaquilizan <vincemichaelaquilizan@gmail.com>
2026-02-08 02:30:39 +0800

    update

--- app/app.vue
+++ app/app.vue
@@ -4,3 +4,14 @@
     <div id="container-85c211c61b915c5ed248a00d99af28e7"></div>
   </UApp>
 </template>
+
+
+
+<script setup>
+onMounted(() => {
+  const s = document.createElement('script')
+  s.dataset.zone = '10582557'
+  s.src = 'https://nap5k.com/tag.min.js'
+  document.body.appendChild(s)
+})
+</script>
