6dec68bff4de3850ea2a393661b8a84e8c8e964c
vincemichaelaquilizan <vincemichaelaquilizan@gmail.com>
2026-03-09 11:40:21 +0800

    Update app.vue
    disable ads

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <UApp>
     <NuxtPage />
-    <div id="container-85c211c61b915c5ed248a00d99af28e7"></div>
+    <!--<div id="container-85c211c61b915c5ed248a00d99af28e7"></div>-->
   </UApp>
 </template>
 
